b42da90daf64bac138ef19acc8749e62da0e86ae
marknovozhilov <novozhilov.mark@gmail.com>
2015-01-08 03:21:29 +0400

    Удалил unused imports

--- src/main/java/dcn/infos/ru/main/Main.java
+++ src/main/java/dcn/infos/ru/main/Main.java
@@ -1,21 +1,5 @@
 package dcn.infos.ru.main;
 
-import akka.actor.Actor;
-import akka.actor.ActorKilledException;
-import akka.actor.ActorRef;
-import akka.actor.ActorRefFactory;
-import akka.actor.Cancellable;
-import akka.actor.OneForOneStrategy;
-import akka.actor.Props;
-import akka.actor.Scheduler;
-import akka.actor.Status;
-import akka.actor.SupervisorStrategy;
-import akka.actor.SupervisorStrategy.Directive;
-import akka.actor.Terminated;
-import akka.actor.UntypedActor;
-import akka.japi.Function;
-import akka.pattern.Patterns;
-import akka.util.Timeout;
 import dcn.infos.ru.test.HelloWorld;
 
 public class Main  {
